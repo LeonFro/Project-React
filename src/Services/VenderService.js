c83a3281419f22a29bae775709b98170611ff836
Anton Yakovets <jokerosky@gmail.com>
2018-03-10 17:38:25 +0600

    merge with origin

--- src/Services/VenderService.js
+++ src/Services/VenderService.js
@@ -2,7 +2,6 @@ export class VenderService {
     database;
     constructor(db) {
         this.database = db;
-<<<<<<< HEAD
         this.deleteVender = this.deleteVender.bind(this);
     }
     
@@ -31,28 +30,4 @@ export class VenderService {
         }
        
     }
-=======
-     this.deleteVender=this.deleteVender.bind(this);
-    }
-    add(vender){
-        
-     this.database.dataVender.push({
-        id: Date.now(),
-        vender: vender
-      });
-    }
-
-    getAll(){
-       return this.database.dataVender;
-    }
-
-    deleteVender(id){  debugger;      
-       let index = this.database.dataVender.findIndex(x=>{
-            return x.id ==id
-        })
-         this.database.dataVender.splice(index, 1 ); 
-           
-      }
-
->>>>>>> 6299b9c445f45a5ea02c587f30cc1e7d7c3b05a3
 } 
